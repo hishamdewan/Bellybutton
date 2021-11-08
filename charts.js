f7a59615535e9a25671c8dae32acb700acb47dc7
Hisham Dewan <88994059+hishamdewan@users.noreply.github.com>
2021-11-08 00:57:12 -0800

    Added bubble chart

--- charts.js
+++ charts.js
@@ -53,6 +53,8 @@ function buildMetadata(sample) {
   });
 }
 
+
+
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
@@ -98,5 +100,54 @@ function buildCharts(sample) {
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("plot", barData, barLayout)      
+
+// Deliverable 2: Create a Bubble Chart 
+    // 1. Create the trace for the bubble chart https://plotly.com/javascript/bubble-charts/.
+    var bubbleData = [{
+      x: ids,
+      y: values,
+      text: labels,
+      mode: 'markers',
+      marker: {
+        color: ids,
+        size: values}
+    }];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+    title: 'Bacteria Cultures Per Sample',
+    xaxis: {title: "OTU ID"},
+    automargins: true, 
+    hovermode: "closest",
+    showlegend: false,
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
+
+    // Deliverable 3: Create a Gauge Chart https://plotly.com/javascript/gauge-charts/
+
+    // 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    var metadata = data.metadata;
+    var gaugeArray = metadata.filter(sampleObj => sampleObj.id == sample);
+
+    // 2. Create a variable that holds the first sample in the metadata array.
+    var gaugeResult = gaugeArray[0];
+ 
+    // 3. Create a variable that holds the washing frequency.
+    var washingFrequency = gaugeResult.wfreq;
+
+    // 4. Create the trace for the gauge chart.
+    var gaugeData = [
+     
+    ];
+    
+    // 5. Create the layout for the gauge chart.
+    var gaugeLayout = { 
+     
+    };
+
+    // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot();
   });
 }
